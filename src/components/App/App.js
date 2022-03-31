2edf2aa5c2b2bf450eff9c8cc50b148ff3e941ee
omid-front <khosrowshahli.omid@gmail.com>
2022-04-01 01:03:19 +0430

    third commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,33 +9,12 @@ function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
   const [whichModal, setWhichModal] = useState({edit: false, delete: false, newData: false});
-  const [newData, setNewData] = useState({id:"", userId: "", title: "", body: ""});
-  const [whichId, setWhichId] = useState(undefined);
+  const [whichId, setWhichId] = useState(0);
 
   const handleNewData = () => {
     setIsModalVisible(true);
     setModalTitle("افزودن اطلاعات");
     setWhichModal({edit: false, delete: false, newData: true});
-  }
-
-  const handleNewId = (id) => {
-    const dataWithNewInfo = {...newData, id};
-    setNewData(dataWithNewInfo);
-  }
-
-  const handleNewUserId = (userId) => {
-    const dataWithNewInfo = {...newData, userId};
-    setNewData(dataWithNewInfo);
-  }
-
-  const handleNewTitle = (title) => {
-    const dataWithNewInfo = {...newData, title};
-    setNewData(dataWithNewInfo);
-  }
-
-  const handleNewBody = (body) => {
-    const dataWithNewInfo = {...newData, body};
-    setNewData(dataWithNewInfo);
   }
 
   return (
@@ -48,32 +27,8 @@ function App() {
       />
       <MyModal
       isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible}
-      modalTitle ={modalTitle} whichModal={whichModal} setData={setData}
-      setNewData={setNewData} newData={newData} whichId={whichId}
+      modalTitle ={modalTitle} whichModal={whichModal} setData={setData} whichId={whichId} data={data}
       >
-        {whichModal.delete && <p>در صورت تایید، اطلاعات به طور دائم حدف می شوند</p>}
-
-        {whichModal.edit &&
-        <div className='edit-modal'>
-          <label>شناسه کاربر</label>
-          <input type='text' onChange={(event) => handleNewUserId(event.target.value)} />
-          <label>عنوان</label>
-          <input type='text' onChange={(event) => handleNewTitle(event.target.value)} />
-          <label>متن</label>
-          <textarea onChange={(event) => handleNewBody(event.target.value)}></textarea>
-        </div>}
-
-        {whichModal.newData &&
-          <div className='newData-modal'>
-            <label>شناسه</label>
-            <input type='text' onChange={(event) => handleNewId(event.target.value)} />
-            <label>شناسه کاربر</label>
-            <input type='text' onChange={(event) => handleNewUserId(event.target.value)} />
-            <label>عنوان</label>
-            <input type='text' onChange={(event) => handleNewTitle(event.target.value)} />
-            <label>متن</label>
-            <textarea onChange={(event) => handleNewBody(event.target.value)}></textarea>
-          </div>}
       </MyModal>
     </div>
   );
--- src/components/MyModal/MyModal.js
+++ src/components/MyModal/MyModal.js
@@ -1,7 +1,12 @@
 import './MyModal.css';
+import { useState } from 'react';
 import { Modal } from 'antd';
 
-const MyModal = ({children, isModalVisible, setIsModalVisible, modalTitle, whichModal, setData , newData, setNewData, whichId}) => {
+const MyModal = ({isModalVisible, setIsModalVisible, modalTitle, whichModal, data, setData , whichId}) => {
+  const [newData, setNewData] = useState({id:"", userId: "", title: "", body: ""});
+  const [isIdEntered, setIsIdEntered] = useState(false);
+  const [message, setMessage] = useState(false);
+  
   const getModifiedData = async () => {
     const response = await fetch(`http://localhost:3000/posts`);
     const data = await response.json();
@@ -22,6 +27,14 @@ const MyModal = ({children, isModalVisible, setIsModalVisible, modalTitle, which
     }
 
     if(whichModal.newData) {
+      if(!isIdEntered) {
+        setMessage(true);
+        return;
+      }
+      else {
+        setMessage(false);
+      }
+
       (async () => {
         await fetch(`http://localhost:3000/posts`, {
           method: 'POST',
@@ -57,10 +70,66 @@ const MyModal = ({children, isModalVisible, setIsModalVisible, modalTitle, which
   const handleCancle = () => {
     setIsModalVisible(false);
   }
+
+  const handleNewId = (id) => {
+    const isIdUnique = data.map((item) => (
+      id === item.id ? true : false
+    ));
+
+    if(isIdUnique.includes(true) || (!id.length))
+    {
+      setIsIdEntered(false);
+      setMessage(true);
+    }
+    else {
+      const dataWithNewInfo = {...newData, id};
+      setNewData(dataWithNewInfo);
+      setIsIdEntered(true);
+      setMessage(false);
+      }
+  }
+
+  const handleNewUserId = (userId) => {
+    const dataWithNewInfo = {...newData, userId};
+    setNewData(dataWithNewInfo);
+  }
+
+  const handleNewTitle = (title) => {
+    const dataWithNewInfo = {...newData, title};
+    setNewData(dataWithNewInfo);
+  }
+
+  const handleNewBody = (body) => {
+    const dataWithNewInfo = {...newData, body};
+    setNewData(dataWithNewInfo);
+  }
   
   return (
     <Modal title={modalTitle} visible={isModalVisible} onOk={() => handleOk(whichId)} onCancel={handleCancle}>
-      {children}
+      {whichModal.delete && <p>در صورت تایید، اطلاعات به طور دائم حدف می شوند</p>}
+
+      {whichModal.edit &&
+        <div className='edit-modal'>
+          <label>شناسه کاربر</label>
+          <input type='text' onChange={(event) => handleNewUserId(event.target.value)} />
+          <label>عنوان</label>
+          <input type='text' onChange={(event) => handleNewTitle(event.target.value)} />
+          <label>متن</label>
+          <textarea onChange={(event) => handleNewBody(event.target.value)}></textarea>
+        </div>}
+
+      {whichModal.newData &&
+        <div className='newData-modal'>
+          <label>شناسه</label>
+          <input type='number' onChange={(event) => handleNewId(event.target.value)} />
+          {message && <p style={{color: 'red'}}>Please enter a unique id</p>}
+          <label>شناسه کاربر</label>
+          <input type='text' onChange={(event) => handleNewUserId(event.target.value)} />
+          <label>عنوان</label>
+          <input type='text' onChange={(event) => handleNewTitle(event.target.value)} />
+          <label>متن</label>
+          <textarea onChange={(event) => handleNewBody(event.target.value)}></textarea>
+        </div>}
     </Modal>
   );
 }
